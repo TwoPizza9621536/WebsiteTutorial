783ac2fb02110bbd4caf4ed9f7e4979b7d8365cd
Samuel Wu 吴申榕 <twopizza9621536@gmail.com>
2022-01-08 15:51:37 -0500

    Change license to CC0 and make it reuse compilent
    Signed-off-by: Samuel Wu 吴申榕 <twopizza9621536@gmail.com>

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
-'use strict'
+/* Start SDPX identifier expressions
+ * SPDX-FileCopyrightText: 2020 Samuel Wu
+ * SPDX-License-Identifier: CC0-1.0
+ * End SDPX identifier expressions
+ */
+"use strict";
 
 let today = new Date();
 let formatDate = today.toDateString();
-let selectElement = document.getElementById('date');
+let selectElement = document.getElementById("date");
 selectElement.innerHTML = formatDate;
 
-console.log('Here\'s a hidden message');
+console.log("Here's a hidden message");
